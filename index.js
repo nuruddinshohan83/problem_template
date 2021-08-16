09398b9f66d4b7f6acfa25a05be2a8e14b4aeeb1
Md Nur Uddin Shohan <nuruddin.shohan@yahoo.com>
2021-08-16 11:45:39 +0600

    A simple tempate for node js

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readline() {
+    return inputString[currentLine++];
+}
+// Make a Snippet for the code above this and then write your logic in main();
+
+
+function main() {
+    let testcase = parseInt(readline())
+    while(testcase--){
+        console.log(testcase)
+    }
+    
+}
