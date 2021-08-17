ecfc23b80ba41d7793ec868d3ecd985afc28a5dc
Md Nur Uddin Shohan <nuruddin.shohan@yahoo.com>
2021-08-17 10:30:47 +0600

    Simpler template

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-'use strict';
+const readLine = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+const input = []
+readLine.on('line', line => input.push(line))
 
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readline() {
-    return inputString[currentLine++];
-}
-// Make a Snippet for the code above this and then write your logic in main();
-
-
-function main() {
-    let testcase = parseInt(readline())
-    while(testcase--){
-        console.log(testcase)
-    }
-    
-}
+readLine.on('close', () => {
+    console.log(input)
+})
